6e80a33f6bd779434389547eb9821d076bb3caa1
kartal5 <mainn2@hotmail.com>
2025-04-05 15:16:02 +0200

    vercel deployment issue fixed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'utfs.io',
-        port: '',
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
