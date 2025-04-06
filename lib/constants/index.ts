ad289f810fd1a202140e42f1772617d00e33e517
kartal5 <mainn2@hotmail.com>
2025-04-06 11:52:40 +0200

    new Prisma schema for reviews, ran migration for new table then created Zod schema with default values

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -48,3 +48,9 @@ export const DEFAULT_PAYMENT_METHOD =
   export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(', ')
   : ['admin', 'user'];
+
+  export const reviewFormDefaultValues = {
+    title: '',
+    comment: '',
+    rating: 0,
+  };
