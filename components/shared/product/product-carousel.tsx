f3800adfb05f929a9d25c2efa345d764419d8c72
kartal5 <mainn2@hotmail.com>
2025-05-01 17:04:10 +0200

    product replacements

--- components/shared/product/product-carousel.tsx
+++ components/shared/product/product-carousel.tsx
@@ -28,18 +28,21 @@ const ProductCarousel = ({ data }: { data: Product[] }) => {
       ]}
     >
       <CarouselContent>
-        {data.map((product: Product) => (
-          <CarouselItem key={product.id}>
-            <Link href={`/product/${product.slug}`}>
-              <div className='relative mx-auto'>
-                <Image
-                  src={product.banner!}
-                  alt={product.name}
-                  height='0'
-                  width='0'
-                  sizes='100vw'
-                  className='w-full h-auto'
-                />
+        {data
+          // skip products that don’t actually have a usable banner
+          .filter((p) => p.banner)          
+          .map((product) => (
+             <CarouselItem key={product.id}>
+               <Link href={`/product/${product.slug}`}>
+                 <div className='relative mx-auto'>
+                  <Image                       
+                    src={product.banner!}       
+                     alt={product.name}
+                     height={0}
+                     width={0}
+                     sizes='100vw'
+                     className='w-full h-auto'
+                   />
                 <div className='absolute inset-0 flex items-end justify-center'>
                   <h2 className='bg-gray-900 bg-opacity-50 text-2xl font-bold px-2 text-white'>
                     {product.name}
